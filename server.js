bf8c1944ae5464464eaa4ae621c05a214883c38e
karthgaibalan <talktokb@gmail.com>
2016-09-19 20:04:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,16 @@ app.get('/article-one', function(req,res)
     res.send('Article-one requested');
 });
 
+app.get('/article-two', function(req,res)
+{
+    res.send('Article-twp requested');
+});
+
+app.get('/article-three', function(req,res)
+{
+    res.send('Article-three requested');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
