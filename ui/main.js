801507accdb10da3c74733a7b858f2dc0924d851
karthgaibalan <talktokb@gmail.com>
2016-09-25 19:00:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,12 @@ console.log('Loaded!');
 //change text
 var element = document.getElementById('maintext');
 element.innerHTML = 'new value';
+
+//Move Image
+
+var element = document.getElementById('madi');
+
+element.onclick=function(){
+    
+    element.style.marginleft = '100px';
+};
