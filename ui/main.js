26eac006f5fb65c98d6105bcf18ed4b3b4b7c69a
karthgaibalan <talktokb@gmail.com>
2016-09-25 19:04:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,10 @@
 console.log('Loaded!');
 
-//change text
-var element = document.getElementById('maintext');
-element.innerHTML = 'new value';
-
 //Move Image
 
 var element = document.getElementById('madi');
 
 element.onclick=function(){
     
-    element.style.marginleft = '100px';
+    element.style.marginleft = '500pxx';
 };
